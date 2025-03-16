f8dc388447eae70dbfb8be12acf16a18d2ec2f58
Chitta Ranjan Behera <chitta_behera@hotmail.com>
2025-03-16 12:14:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3003;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Node js app!');
 });
 
 app.listen(PORT, () => {
